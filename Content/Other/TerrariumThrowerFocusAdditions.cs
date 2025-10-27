1ede4aa645b55fb5e1e9b4de9d33bbb1fa50c63c
Darian Hughes <darianmhughes7@gmail.com>
2025-10-27 18:20:43 -0500

    Thorium's Terrarium Armor Thrower Focus now gives stealth when Calamity is enabled.

---
+++ Content/Other/TerrariumThrowerFocusAdditions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using CalamityMod;
+using MonoMod.RuntimeDetour;
+using Terraria;
+using Terraria.ModLoader;
+using ThoriumMod.Items.Terrarium;
+
+namespace ThrowerUnification.Content.Other
+{
+    [JITWhenModsEnabled(ModCompatibility.Thorium.Name)]
+    [ExtendsFromMod(ModCompatibility.Thorium.Name)]
+    public class TerrariumThrowerFocusAdditions : ModSystem
+    {
+        private static Hook throwingEffectHook;
+
+        public override void Load()
+        {
+            // Find the TerrariumHelmet.ThrowingEffect method via reflection
+            MethodInfo orig = typeof(TerrariumHelmet)
+                .GetMethod("ThrowingEffect", BindingFlags.Public | BindingFlags.Static);
+
+            // Create the hook
+            throwingEffectHook = new Hook(orig, (Action<Player> origMethod, Player player) =>
+            {
+                // Call original
+                origMethod(player);
+
+                // Inject
+                if (ModCompatibility.Calamity.Loaded)
+                {
+                    AddCalamityStealth.AddStealth(player);
+                }
+            });
+        }
+
+        public override void Unload()
+        {
+            throwingEffectHook?.Dispose();
+            throwingEffectHook = null;
+        }
+    }
+
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public static class AddCalamityStealth
+    {
+        public static void AddStealth(Player player)
+        {
+            var modPlayer = player.Calamity();
+            modPlayer.rogueStealthMax += 1.10f;
+            modPlayer.wearingRogueArmor = true;
+        }
+    }
+}
--- obj/Debug/net8.0/ThrowerUnification.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThrowerUnification.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThrowerUnification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91c3f783431f29d5a95ad2b09bea59711ce9ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f96417fc9cfd87123fed17e69e371ff41ee5b335")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThrowerUnification")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThrowerUnification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
