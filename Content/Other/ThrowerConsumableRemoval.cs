c2ba0ecc2a7b93fb40a5f112a5a27636f0f49edc
Joshua D'Abreu <JD250657@Falmouth.ac.uk>
2026-06-10 17:01:40 +0100

    Recipe adjustment fixes

--- Content/Other/ThrowerConsumableRemoval.cs
+++ Content/Other/ThrowerConsumableRemoval.cs
@@ -169,21 +169,22 @@ namespace ThrowerUnification.Content.Other
 
         public override void PostAddRecipes()
         {
-            for (int i = 0; i < Main.recipe.Length; i++)
-            {
-                Recipe recipe = Main.recipe[i];
+            List<Recipe> recipesToReplace = new();
 
+            foreach (Recipe recipe in Main.recipe)
+            {
                 if (recipe == null)
                     continue;
 
-                bool createsUnifiedThrowable = UnifiedThrowableGlobalItem.OriginallyConsumableThrowableTypes.Contains(recipe.createItem.type);
+                bool modifiesRecipe = false;
 
-                if (!createsUnifiedThrowable)
-                    continue;
+                // Crafts a formerly consumable throwable
+                if (UnifiedThrowableGlobalItem.OriginallyConsumableThrowableTypes.Contains(recipe.createItem.type))
+                {
+                    modifiesRecipe = true;
+                }
 
-                // Force output to 1
-                recipe.createItem.stack = 1;
-
+                // Uses a unified throwable ingredient
                 foreach (Item ingredient in recipe.requiredItem)
                 {
                     if (ingredient == null || ingredient.type <= ItemID.None)
@@ -191,13 +192,61 @@ namespace ThrowerUnification.Content.Other
 
                     if (UnifiedThrowableGlobalItem.UnifiedThrowableTypes.Contains(ingredient.type))
                     {
-                        ingredient.stack = 1;
+                        modifiesRecipe = true;
+                        break;
+                    }
+                }
+
+                if (modifiesRecipe)
+                {
+                    recipesToReplace.Add(recipe);
+                }
+            }
+
+            foreach (Recipe oldRecipe in recipesToReplace)
+            {
+                bool createsUnifiedThrowable =
+                    UnifiedThrowableGlobalItem.OriginallyConsumableThrowableTypes.Contains(oldRecipe.createItem.type);
+
+                int resultStack = createsUnifiedThrowable
+                    ? 1
+                    : oldRecipe.createItem.stack;
+
+                Recipe newRecipe = Recipe.Create(oldRecipe.createItem.type, resultStack);
+
+                foreach (Item ingredient in oldRecipe.requiredItem)
+                {
+                    if (ingredient == null || ingredient.type <= ItemID.None)
+                        continue;
+
+                    int stack;
+
+                    if (UnifiedThrowableGlobalItem.UnifiedThrowableTypes.Contains(ingredient.type))
+                    {
+                        // old behavior: unified throwable ingredients cost 1
+                        stack = 1;
+                    }
+                    else if (createsUnifiedThrowable)
+                    {
+                        // old behavior: recipes crafting former consumables cost 4x other mats
+                        stack = ingredient.stack * 4;
                     }
                     else
                     {
-                        ingredient.stack *= 4;
+                        // recipes merely using a unified throwable keep normal costs
+                        stack = ingredient.stack;
                     }
+
+                    newRecipe.AddIngredient(ingredient.type, stack);
                 }
+
+                foreach (int tile in oldRecipe.requiredTile)
+                {
+                    newRecipe.AddTile(tile);
+                }
+
+                newRecipe.Register();
+                oldRecipe.DisableRecipe();
             }
         }
     }
