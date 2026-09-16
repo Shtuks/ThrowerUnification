b2b76b73c0df47394c4d9cda004c946153b007d1
Darian Hughes <darianmhughes7@gmail.com>
2026-09-15 22:45:44 -0500

    minor buff adjustment

---
+++ Content/Buffs/ThrowingSpeedBuffAdjustment.cs
@@ -0,0 +1,21 @@
+﻿using CalamityMod.CalPlayer;
+using Terraria;
+using Terraria.ModLoader;
+using ThoriumMod.Buffs.Thrower;
+
+namespace ThrowerUnification.Content.Buffs
+{
+    [JITWhenModsEnabled(ModCompatibility.Thorium.Name, ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Thorium.Name, ModCompatibility.Calamity.Name)]
+    public class ThrowingSpeedBuffAdjustment : GlobalBuff
+    {
+        public override void Update(int type, Player player, ref int buffIndex)
+        {
+            if (type == ModContent.BuffType<ThrowingSpeed>())
+            {
+                player.GetAttackSpeed(DamageClass.Throwing) -= 0.15f;
+                player.GetModPlayer<CalamityPlayer>().rogueVelocity += 0.15f;
+            }
+        }
+    }
+}
